ade547eb50d3ce85e0b53b568dd10bdc35169d96
Danil <dardrijeri@gmail.com>
2024-04-03 20:22:29 +0300

    Added api for viewing task and descendants

--- training_project/tasks/views.py
+++ training_project/tasks/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.core import serializers
+from django.shortcuts import render, HttpResponse
 from tasks.models import Task
 
 
 def index(request):
     return render(request, 'tasks/index.html', {'tasks': Task.objects.all()})
+
+
+def api(request):
+    id = request.GET['id']
+    try:
+        data = serializers.serialize('json', Task.objects.get(pk=id).get_descendants(include_self=True))
+    except Task.DoesNotExist:
+        return HttpResponse("Invalid id")
+    return HttpResponse(data, content_type='application/json')
