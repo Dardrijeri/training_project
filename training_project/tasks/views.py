bff620037d6f751939cd8de869345be665ce76d9
Danil <dardrijeri@gmail.com>
2024-04-04 16:50:03 +0300

    Added workaround for cors

--- training_project/tasks/views.py
+++ training_project/tasks/views.py
@@ -8,9 +8,17 @@ def index(request):
 
 
 def api(request):
-    id = request.GET['id']
+    task_id = request.GET['id']
     try:
-        data = serializers.serialize('json', Task.objects.get(pk=id).get_descendants(include_self=True))
+        data = serializers.serialize('json', Task.objects.get(pk=task_id).get_descendants(include_self=True))
     except Task.DoesNotExist:
         return HttpResponse("Invalid id")
-    return HttpResponse(data, content_type='application/json')
+    response = HttpResponse(data, content_type='application/json')
+
+    # workaround to avoid including cors library
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+
+    return response
